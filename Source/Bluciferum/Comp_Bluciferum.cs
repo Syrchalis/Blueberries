a28777bea14b77b83e181d4619a079c4ffa977d1
Syrchalis <syrchalis@gmx.de>
2020-08-09 20:55:37 +0200

    1.2 Update

--- Source/Bluciferum/Comp_Bluciferum.cs
+++ Source/Bluciferum/Comp_Bluciferum.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using RimWorld;
 using Verse;
+using System.Reflection;
 
 namespace SyrBlueberries
 {
@@ -16,25 +17,25 @@ namespace SyrBlueberries
             if (ingester.health.hediffSet.HasHediff(InfectionDefOf.MuscleParasites))
             {
                 HediffWithComps hediff = (HediffWithComps)ingester.health.hediffSet.GetFirstHediffOfDef(InfectionDefOf.MuscleParasites);
-                hediff.comps.First(h => h is HediffComp_TendDuration).CompTended(1.25f, 0);
+                hediff.comps.First(h => h is HediffComp_TendDuration).CompTended_NewTemp(1.25f, 0);
             }
             if (ingester.health.hediffSet.HasHediff(InfectionDefOf.GutWorms))
             {
                 HediffWithComps hediff = (HediffWithComps)ingester.health.hediffSet.GetFirstHediffOfDef(InfectionDefOf.GutWorms);
-                hediff.comps.First(h => h is HediffComp_TendDuration).CompTended(1.25f, 0);
+                hediff.comps.First(h => h is HediffComp_TendDuration).CompTended_NewTemp(1.25f, 0);
             }
             if (ingester.health.hediffSet.HasHediff(InfectionDefOf.FibrousMechanites))
             {
-                HediffWithComps hediff = (HediffWithComps)ingester.health.hediffSet.GetFirstHediffOfDef(InfectionDefOf.FibrousMechanites);
-                HediffComp hediffComp = hediff.comps.First(h => h is HediffComp_Disappears);
+                Hediff hediff = ingester.health.hediffSet.GetFirstHediffOfDef(InfectionDefOf.FibrousMechanites);
+                HediffComp hediffComp = hediff.TryGetComp<HediffComp_Disappears>();
                 Traverse field = Traverse.Create(hediffComp).Field("ticksToDisappear");
                 int ticksToDisappear = field.GetValue<int>();
                 field.SetValue(ticksToDisappear - 180000);
             }
             if (ingester.health.hediffSet.HasHediff(InfectionDefOf.SensoryMechanites))
             {
-                HediffWithComps hediff = (HediffWithComps)ingester.health.hediffSet.GetFirstHediffOfDef(InfectionDefOf.SensoryMechanites);
-                HediffComp hediffComp = hediff.comps.First(h => h is HediffComp_Disappears);
+                Hediff hediff = ingester.health.hediffSet.GetFirstHediffOfDef(InfectionDefOf.SensoryMechanites);
+                HediffComp hediffComp = hediff.TryGetComp<HediffComp_Disappears>();
                 Traverse field = Traverse.Create(hediffComp).Field("ticksToDisappear");
                 int ticksToDisappear = field.GetValue<int>();
                 field.SetValue(ticksToDisappear - 180000);
@@ -43,7 +44,6 @@ namespace SyrBlueberries
             {
                 ingester.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.FoodPoisoning).Heal(1);
             }
-
         }
     }
 }
